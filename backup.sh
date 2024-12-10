0b63ec6efb03e00205d1fdb769b9e2915bbc3a44
anyshpm <anyshpm@anyshpm.com>
2024-12-10 01:14:57 +0000

    Add OLD_DAYS_TO_DELETE to set days.

--- backup.sh
+++ backup.sh
@@ -5,6 +5,7 @@ BACKUP_DRIVE_NAME=${BACKUP_DRIVE_NAME:-cloud}
 BACKUP_DIR=${BACKUP_DIR:-/data}
 BACKUP_USERNAME=${BACKUP_USERNAME:-user}
 BACKUP_DRIVE_PATH=${BACKUP_DRIVE_PATH:-/rclone/backups/$HOSTNAME/$BACKUP_USERNAME/daily}
+OLD_DAYS_TO_DELETE=${OLD_DAYS_TO_DELETE}
 
 # 检查备份目录是否存在
 if [ ! -d "$BACKUP_DIR" ]; then
@@ -34,9 +35,12 @@ DATE=$(date +%Y%m%d%H%M)
 tar --exclude-from="$BACKUP_DIR/.kopiaignore" -czf - "$BACKUP_DIR" | rclone rcat "$BACKUP_DRIVE_NAME:$BACKUP_DRIVE_PATH/backup-$DATE.tar.gz" || { echo "Failed to create tar archive" >&2; exit 1; }
 echo "Tar archive created and uploaded successfully"
 
-# 删除远程存储中创建时间超过7天的文件
-rclone delete "$BACKUP_DRIVE_NAME:$BACKUP_DRIVE_PATH" --min-age 7d || { echo "Failed to delete old backups" >&2; exit 1; }
-echo "Old backups deleted successfully"
+# 删除远程存储中创建时间超期的文件
+if [ -n "$OLD_DAYS_TO_DELETE" ]
+then
+    rclone delete "$BACKUP_DRIVE_NAME:$BACKUP_DRIVE_PATH" --min-age ${OLD_DAYS_TO_DELETE}d || { echo "Failed to delete old backups" >&2; exit 1; }
+    echo "Old backups deleted successfully"
+fi
 
 # 备份完成
 echo "Backup completed successfully"
